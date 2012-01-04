2784a07764c90dfda0b84ea2014a7cc030793ef0
David Walter Seikel <won_fang@yahoo.com.au>
2012-01-05 09:08:13 +1000

    Bit more cleanup.

--- LuaSL/src/LuaSL_parser.c
+++ LuaSL/src/LuaSL_parser.c
@@ -4,14 +4,16 @@
 
 int main(void)
 {
-    char test[]=" 4 + 2*10 + 3*( 5 + 1 )";
+    const char test[] = " 4 + 2*10 + 3*( 5 + 1 )";
     SExpression *e = NULL;
-    int result = 0;
 
-    e = getAST(test);
-    result = evaluate(e);
-    printf("Result of '%s' is %d\n", test, result);
-    deleteExpression(e);
+    if ((e = getAST(test)))
+    {
+	int result = evaluate(e);
+
+	printf("Result of '%s' is %d\n", test, result);
+	deleteExpression(e);
+    }
 
     return 0;
 }
