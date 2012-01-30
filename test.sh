0c0a43ad00d32c99c8da43a2f7524a2a37c9803d
David Walter Seikel <won_fang@yahoo.com.au>
2012-01-30 13:17:18 +1000

    Make it easier to inkoke a debugger.

--- test.sh
+++ test.sh
@@ -10,5 +10,21 @@ echo "_______________ TESTING LuaSL _______________"
 cd $wd/LuaSL/testLua
 export LUA_SOPATH='../../libraries/luaproc/' 
 export LD_LIBRARY_PATH="../../libraries/luajit-2.0/src:$LD_LIBRARY_PATH"
-../LuaSL
+
+
+case $@ in
+
+    ddd)
+	ddd ../LuaSL
+	;;
+
+    gdb)
+	gdb ../LuaSL
+	;;
+
+    *)
+	../LuaSL
+	;;
+
+esac
 
