0626bc79168a4735df9083be616efb05bb106de6
David Walter Seikel <won_fang@yahoo.com.au>
2012-02-04 21:52:48 +1000

    Cooment a problem with typecasting strings to integers.

--- LuaSL/src/LuaSL_compile.c
+++ LuaSL/src/LuaSL_compile.c
@@ -1,5 +1,23 @@
 #include "LuaSL.h"
 
+/* TODO -
+Bit of a problem, but I think it will go away once llGetSubString() is implemented.
+The problem is that Lua can't convert the string "0x" to a number, though it's fine with "0x0".
+In LSL any string that does not represent a number gets converted to 0 by a typecast.
+
+In other cases the LSL code is typecasting a string to an integer, but Lua can't coz it's not a real number in the string.
+
+Sooo, should actually implement that typecast.
+
+function  --[[integer]] channel()
+  return  --[[(integer)]] ("0x" .. _LSL.llGetSubString( --[[(string)]] _LSL.llGetKey(), -4, -1));
+end 
+
+function setBalls( --[[string]] cmd)
+  local  --[[integer]] ch=channel();
+  _LSL.llSay(ch+ix, cmd);
+end  
+*/
 
 
 static LSL_Leaf *evaluateFloatToken(LSL_Leaf  *content, LSL_Leaf *left, LSL_Leaf *right);
