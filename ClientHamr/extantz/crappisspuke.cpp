d2284fac245442857ac153db2c99b1f4134a24ce
David Walter Seikel <won_fang@yahoo.com.au>
2013-01-20 03:41:45 +1000

    Move the Irrlicht namespace stuff into the extantz header.

--- ClientHamr/extantz/crappisspuke.cpp
+++ ClientHamr/extantz/crappisspuke.cpp
@@ -1,30 +1,5 @@
 
 #include <irrlicht.h>
-
-/*
-In the Irrlicht Engine, everything can be found in the namespace 'irr'. So if
-you want to use a class of the engine, you have to write irr:: before the name
-of the class. For example to use the IrrlichtDevice write: irr::IrrlichtDevice.
-To get rid of the irr:: in front of the name of every class, we tell the
-compiler that we use that namespace from now on, and we will not have to write
-irr:: anymore.
-*/
-using namespace irr;
-
-/*
-There are 5 sub namespaces in the Irrlicht Engine. Take a look at them, you can
-read a detailed description of them in the documentation by clicking on the top
-menu item 'Namespace List' or by using this link:
-http://irrlicht.sourceforge.net/docu/namespaces.html
-Like the irr namespace, we do not want these 5 sub namespaces now, to keep this
-example simple. Hence, we tell the compiler again that we do not want always to
-write their names.
-*/
-using namespace core;
-using namespace scene;
-using namespace video;
-
-// This is here so that we can use the name spacing above, and not have to #ifdef lots of shit.
 #include "extantz.h"
 
 
