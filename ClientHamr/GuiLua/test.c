47f6d4c230dd8f1eeea19df48f74cd649cdc039c
David Walter Seikel <won_fang@yahoo.com.au>
2014-03-19 21:08:57 +1000

    Add some notes to test.c.

--- ClientHamr/GuiLua/test.c
+++ ClientHamr/GuiLua/test.c
@@ -17,3 +17,22 @@ end)
 return result;
 
 */
+
+
+
+/* NOTES -
+
+From http://www.inf.puc-rio.br/~roberto/pil2/chapter15.pdf
+
+"Well-behaved C libraries should export one function called
+luaopen_modname, which is the function that require tries to call after
+linking the library.  In Section 26.2 we will discuss how to write C
+libraries."
+
+The "modname" bit is replaced by the name of the module.  Though if the
+module name includes a hyphen, the "require" function strips out the
+hyphen and the bit before it.
+
+Though it seems that chapter 26 is not in the same place?
+
+*/
