4f788029394d84a839a72c24b637ba557514f2c7
David Walter Seikel <won_fang@yahoo.com.au>
2014-03-19 21:44:04 +1000

    "Simplify" test.c.  B-)

--- ClientHamr/GuiLua/test.c
+++ ClientHamr/GuiLua/test.c
@@ -1,20 +1,4 @@
-/* Should be a Lua module, roughly the same as -
-
-local skang = require('skang')
-local result = {};
-result.author = 'onefang'
-result.version = '0.72 alpha 2004-11-19 16:28:00'
-local bar
--- The first argument would be the name of a local variable / method.  Which could be accessed via _G?
--- Not sure if we could use a global bar, AND use it directly.
-result.bar = skang.newParam('bar', "Required", "Shortcut", "Default", "Help text")
-result.func = skang.newCommand('number,data', 'Help Text', function (arg1, arg2)
--- do something here
-end)
-
--- do something here
-
-return result;
+/* Should be a Lua module, roughly the same as test.lua
 
 */
 
