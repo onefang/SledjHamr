86719f3016e41bf6389dd08d577c02c9921321f5
David Walter Seikel <won_fang@yahoo.com.au>
2014-05-04 17:14:18 +1000

    Work around Elm entry hang on exit bug in chat.

--- src/extantz/chat.c
+++ src/extantz/chat.c
@@ -1,6 +1,15 @@
 #include "extantz.h"
 
 
+// TODO - This is to work around a bug in Elm entry, remove it when the bug is fixed.
+//   The bug is that editable entry widgets cause the app to hang on exit.
+void _on_entry_del(void *data, Evas_Object *obj, void *event_info)
+{
+//  fangWin *me = data;
+
+  elm_entry_editable_set(obj, EINA_FALSE);
+}
+
 fangWin *chat_add(globals *ourGlobals)
 {
   fangWin *me;
@@ -29,18 +38,18 @@ fangWin *chat_add(globals *ourGlobals)
   eo_unref(en);
 
   en = eo_add(ELM_OBJ_ENTRY_CLASS, me->win);
+  wid = widgetAdd(me);
+  wid->obj = en;
+  wid->on_del = _on_entry_del;
   elm_object_text_set(en, "");
   eo_do(en,
     elm_obj_entry_scrollable_set(EINA_TRUE),
-// TODO - Setting editable to TRUE is what hangs up extantz on exit.
-// elm_obj_entry_editable_set(EINA_TRUE),
-    elm_obj_entry_editable_set(EINA_FALSE),
+    elm_obj_entry_editable_set(EINA_TRUE),
     evas_obj_size_hint_weight_set(EVAS_HINT_EXPAND, EVAS_HINT_EXPAND),
     evas_obj_size_hint_align_set(EVAS_HINT_FILL, EVAS_HINT_FILL),
     evas_obj_visibility_set(EINA_TRUE)
        );
   elm_box_pack_end(bx, en);
-  eo_unref(en);
 
   evas_object_show(bx);
   eo_unref(bx);
