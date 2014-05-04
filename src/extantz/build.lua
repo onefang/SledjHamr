e159a663602fdcdfd1d6cc89fe9b13f5f2a511b9
David Walter Seikel <won_fang@yahoo.com.au>
2014-05-05 02:40:47 +1000

    Delete libwinFang.so on build.

--- src/extantz/build.lua
+++ src/extantz/build.lua
@@ -18,7 +18,7 @@ LDFLAGS = LDFLAGS .. ' -L../../libraries/irrlicht-1.8.1/lib/Linux'
 libs    = libs    .. ' -lIrrlicht -lGL -lbz2'
 
 removeFiles(dir, {'crappisspuke.o', 'CDemo.o', 'extantzCamera.o', 'gears.o', 'ephysics_demo.o', 'Evas_3D_demo.o', '../../media/extantz.edj'})
-removeFiles(dir, {'../../extantz', 'camera.o', 'winFang.o', 'chat.o', 'files.o', 'woMan.o'})
+removeFiles(dir, {'../../extantz', 'camera.o', 'winFang.o', lib_d .. '/libwinFang.so', 'chat.o', 'files.o', 'woMan.o'})
 
 runCommand('edje_cc',		dir, 'edje_cc ' .. EDJE_FLAGS .. ' extantz.edc ../../media/extantz.edj')
 runCommand('Irrlicht files',	dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c crappisspuke.cpp -o crappisspuke.o ' .. LDFLAGS)
