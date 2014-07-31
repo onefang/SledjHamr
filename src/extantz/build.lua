ad814a58d8b5bc1958861d3debea1a1816651905
David Walter Seikel <won_fang@yahoo.com.au>
2014-07-31 11:19:42 +1000

    Stop building in Irrlicht, but keep it around in case Evas_3D isn't good enough.

--- src/extantz/build.lua
+++ src/extantz/build.lua
@@ -13,15 +13,19 @@ if 'nil' == type(dir) then
   dir = workingDir
 end
 
-CFLAGS  = CFLAGS  .. ' -I../../libraries/irrlicht-1.8.1/include -I/usr/X11R6/include -I../GuiLua'
-LDFLAGS = LDFLAGS .. ' -L../../libraries/irrlicht-1.8.1/lib/Linux'
-libs    = libs    .. ' -lIrrlicht -lephysics -lGL -lbz2 -lGuiLua'
+--CFLAGS  = CFLAGS  .. ' -I../../libraries/irrlicht-1.8.1/include -I/usr/X11R6/include -I../GuiLua'
+--LDFLAGS = LDFLAGS .. ' -L../../libraries/irrlicht-1.8.1/lib/Linux'
+--libs    = libs    .. ' -lIrrlicht -lephysics -lGL -lbz2 -lGuiLua'
+CFLAGS  = CFLAGS  .. ' -I/usr/X11R6/include -I../GuiLua'
+libs    = libs    .. ' -lephysics -lGuiLua'
 
-removeFiles(dir, {'crappisspuke.o', 'CDemo.o', 'extantzCamera.o', '../../media/extantz.edj'})
+--removeFiles(dir, {'crappisspuke.o', 'CDemo.o', 'extantzCamera.o', '../../media/extantz.edj'})
+removeFiles(dir, {'../../media/extantz.edj'})
 
 runCommand('edje_cc',		dir, 'edje_cc ' .. EDJE_FLAGS .. ' extantz.edc ../../media/extantz.edj')
-runCommand('Irrlicht files',	dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c crappisspuke.cpp -o crappisspuke.o ' .. LDFLAGS)
-runCommand(nil,			dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c CDemo.cpp -o CDemo.o ' .. LDFLAGS)
-runCommand(nil,			dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c extantzCamera.cpp -o extantzCamera.o ' .. LDFLAGS)
+--runCommand('Irrlicht files',	dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c crappisspuke.cpp -o crappisspuke.o ' .. LDFLAGS)
+--runCommand(nil,			dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c CDemo.cpp -o CDemo.o ' .. LDFLAGS)
+--runCommand(nil,			dir, 'g++ ' .. CFLAGS .. ' -ffast-math -c extantzCamera.cpp -o extantzCamera.o ' .. LDFLAGS)
 CFLAGS = CFLAGS .. ' -Wl,-export-dynamic'
-compileFiles('../../extantz',	dir, {'gears', 'ephysics_demo', 'camera', 'files', 'scenri', 'woMan', 'extantz'}, 'crappisspuke.o CDemo.o extantzCamera.o')
+--compileFiles('../../extantz',	dir, {'gears', 'ephysics_demo', 'camera', 'files', 'scenri', 'woMan', 'extantz'}, 'crappisspuke.o CDemo.o extantzCamera.o')
+compileFiles('../../extantz',	dir, {'gears', 'ephysics_demo', 'camera', 'files', 'scenri', 'woMan', 'extantz'}, '')
