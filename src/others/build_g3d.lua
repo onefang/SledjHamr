3e67fa7de199257e9f5d473ad14035d6ff1de2e6
David Walter Seikel <won_fang@yahoo.com.au>
2016-03-29 02:40:31 +1000

    Clean up last commit.

--- src/others/build_g3d.lua
+++ src/others/build_g3d.lua
@@ -15,5 +15,5 @@ end
 
 runCommand('libg3d', 'mimesh/libg3d-0.0.8', 'make clean')
 runCommand(nil, 'mimesh/libg3d-0.0.8', './configure && make')
-runCommand('g3dviewer', 'mimesh/g3dviewer-0.2.99.4', './configure && make')
-runCommand(nil, 'mimesh/g3dviewer-0.2.99.4', 'make clean')
+runCommand('g3dviewer', 'mimesh/g3dviewer-0.2.99.4', 'make clean')
+runCommand(nil, 'mimesh/g3dviewer-0.2.99.4', './configure && make')
