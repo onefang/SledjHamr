e6a8af898de06e5dea07c8bd84bc53e5118881ff
David Walter Seikel <won_fang@yahoo.com.au>
2016-03-28 22:18:15 +1000

    Move lemon to the src/others directory.

--- src/LuaSL/build.lua
+++ src/LuaSL/build.lua
@@ -16,7 +16,7 @@ end
 removeFiles(dir, {'*.output', '*.backup', 'LuaSL_lexer.h', 'LuaSL_lexer.c', 'LuaSL_lemon_yaccer.h', 'LuaSL_lemon_yaccer.c', 'LuaSL_lemon_yaccer.out'})
 
 -- Build and run lemon first, flex depends on it to define the symbol values.
-compileFiles('lemon', dir .. '/../../libraries/lemon', {'lemon'}, '')
-runCommand('lemon',   dir, '../../libraries/lemon/lemon -qs -T../../libraries/lemon/lempar.c LuaSL_lemon_yaccer.y')
+compileFiles('lemon', dir .. '/../others/lemon', {'lemon'}, '')
+runCommand('lemon',   dir, '../others/lemon/lemon -qs -T../others/lemon/lempar.c LuaSL_lemon_yaccer.y')
 runCommand('flex',    dir, 'flex -C --outfile=LuaSL_lexer.c --header-file=LuaSL_lexer.h LuaSL_lexer.l')
 compileFiles('../../LuaSL', dir, {'LuaSL_main', 'LuaSL_compile', 'LuaSL_lexer', 'LuaSL_lemon_yaccer'}, '')
